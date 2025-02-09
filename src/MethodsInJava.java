ee46f1993cdb490d29a9d13be69201fa3a846ac7
saurav kumar rajak <sauravrajak@outlook.com>
2025-02-09 15:21:22 +0530

    added functions in MethodesInJava.java

--- src/MethodsInJava.java
+++ src/MethodsInJava.java
@@ -2,9 +2,19 @@ class MethodsInJava{
   public static void main (String []args){
     System.out.println("Hello World!!");
     greetUser();
+    printFirstPattern();
   }
 
   public static void greetUser(){
     System.out.println("Good Morining Saurav");
+  }
+
+  public static  void printFirstPattern(){
+    System.out.println("*");
+    System.out.println("* *");
+    System.out.println("* * *");
+    System.out.println("* * * *");
+    System.out.println("* * * * *");
+    System.out.println("* * * * * *");
   }
 }
